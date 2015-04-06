ec52f60d8d90a920884d4ef09d9c1c8f0cee1518
Jeffrey Ercolani <Jercolani@JeffyMBP.local>
2015-04-06 09:37:28 -0700

    finished project

--- app.js
+++ app.js
@@ -1,34 +1,75 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var pg = require("pg");
+var db = require("./models");
+var methodOverride = require("method-override");
 
 var app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'))
 
 // Refactor connection and query code
-var db = require("./models");
 
 app.get('/articles', function(req,res) {
-  console.log("GET /articles");
+	db.Article.findAll().then(function(articles) {
+		res.render('articles/index', {articlesList: articles});
+	})
+  ;
 });
 
 app.get('/articles/new', function(req,res) {
   res.render('articles/new');
 });
 
+app.get('/articles/:id/edit', function(req,res) {
+  var id = req.params.id;
+  db.Article.find(id).then(function(editting) {res.render('articles/edit', {articleToEdit: editting, id: id})});
+});
+
 app.post('/articles', function(req,res) {
-  console.log(req.body);
+  var title = req.body.articleTitle;
+  var author = req.body.articleAuthor;
+  var content = req.body.articleContent;
+  db.Article.create({title: title, author: author, content: content, fiction: true})
+  .then(function(postArticle) {
+  	res.redirect('/articles')
+	});
 });
 
 app.get('/articles/:id', function(req, res) {
-  console.log(req.body);
+	var id = req.params.id;
+ 	db.Article.find(id).then(function (articles) {res.render('articles/article', {articleToDisplay: articles})});
   
 })
 
+app.delete('/articles/:id', function(req,res) {
+	var id = req.params.id;
+  	db.Article.find(id).then(function(deletedArticle) {
+  		deletedArticle.destroy().then(function(redirect) {
+  			res.redirect('/articles');
+  		});
+  	});
+});
+
+app.put('/articles/:id', function(req,res) {
+  var articleId = req.params.id;
+  var title = req.body.articleTitle;
+  var author = req.body.articleAuthor;
+  var content = req.body.articleContent;
+  db.Article.find(articleId).then(function (article) {
+  	article.updateAttributes({
+  		title: title,
+  		author: author,
+  		content: content}).then(function(edit) {
+  			res.redirect('/articles/'+articleId);
+			});
+		});
+});
+
 app.get('/', function(req,res) {
-  res.render('site/index.ejs');
+  res.render('site/index');
 });
 
 app.get('/about', function(req,res) {
--- schema.sql
+++ schema.sql
@@ -1,12 +1,12 @@
-CREATE DATABASE articles_app;
+CREATE DATABASE newsfeed_app;
 
-\c articles_app
+\c newsfeed_app
 
-CREATE TABLE articles (
+CREATE TABLE Articles (
     id serial primary key,
     title varchar(255),
     author varchar(255),
     content text
   );
 
-ALTER TABLE articles ADD COLUMN fiction boolean;
+ALTER TABLE Articles ADD COLUMN fiction boolean;
